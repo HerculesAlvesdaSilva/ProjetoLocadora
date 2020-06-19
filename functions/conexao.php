07863ea504a1015b624eec60a067d6366d4416db
HerculesAlvesdaSilva <hercules.as09@gmail.com>
2020-06-19 09:25:42 -0300

    Alterações de dados do banco!

--- functions/conexao.php
+++ functions/conexao.php
@@ -1,11 +1,11 @@
 <?php
-
-define("HOST", "localhost");
-define("BD", "locadoraVirtual");
-define("USER", "root");
-define("PASS", "");
+    define("HOST", "localhost");
+    define("BD", "locadoraVirtual");
+    define("USER", "root");
+    define("PASS", "");
 
 function conectarBanco() {
+    
     try {
         $dsn = "mysql:host=" . HOST . ";dbname=" . BD;
         $pdo = new PDO($dsn, USER, PASS);
